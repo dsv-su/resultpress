b98c6e1d8f49db073abbfa6eb02c3d70681a94e1
orkit <ryan@dias.se>
2020-10-17 20:41:51 +0200

    Removed password hash from partner login log

--- app/User.php
+++ app/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
     ];
 
     protected static $logAttributes = [
-        'name', 'email', 'password',
+        'name', 'email',
     ];
     protected static $logOnlyDirty = true;
 

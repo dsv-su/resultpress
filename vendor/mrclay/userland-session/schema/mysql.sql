2c4d129f47a4a92189b96830d9577324aac01931
orkit <ryan@dias.se>
2021-02-18 10:48:41 +0100

    .gitignore is now working

--- vendor/mrclay/userland-session/schema/mysql.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `userland_sessions` (
-   `id` VARCHAR(40) NOT NULL,
-   `data` MEDIUMBLOB,
-   `time` INT NOT NULL,
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- vendor/mrclay/userland-session/src/UserlandSession/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace UserlandSession;
-
-class Exception extends \Exception
-{
-
-}

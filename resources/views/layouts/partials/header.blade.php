4e362968460a53f5d6f228047b027f5729ac9c3c
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2021-02-25 15:44:33 +0300

    Spider logo

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,14 +1,17 @@
 <div id="header">
+    <!--
     <a id="header-su-responsive" href="http://www.su.se/english" title="Stockholm University home">
         <img src="{{ asset('images/su_logo_responsive_en.png') }}" alt="Stockholm University"/>
     </a>
+    -->
 
-    <a id="header-dsv" href="http://dsv.su.se/en/" title="To DSV's start page" accesskey="1">
-        <img src="{{ asset('images/dsv_logo_en.png') }}" alt="Department of Computer and Systems Sciences">
+    <a id="header-dsv" href="#" title="Home" accesskey="1">
+        <img src="{{ asset('images/logo-2015-big-copy.png') }}" alt="SPIDER">
     </a>
-
+<!--
     <a id="header-su" href="http://www.su.se/english/" title="Stockholm University home">
         <img src="{{ asset('images/su_logo_en.gif') }}" alt="Stockholm University">
     </a>
+    -->
     <div class="clear"></div>
 </div>

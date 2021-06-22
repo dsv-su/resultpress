020dd039741040bbb373185fc0980096f9421470
orkit <ryan@dias.se>
2021-06-22 10:27:13 +0200

    Added User accociation with Organisation logging

--- app/UserOrganisation.php
+++ app/UserOrganisation.php
@@ -4,9 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class UserOrganisation extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
     protected $fillable = ['user_id', 'organisation_id'];
 }

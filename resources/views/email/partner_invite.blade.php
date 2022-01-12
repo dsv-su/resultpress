d6c926343687acb3a2761a02ae4436929210b5f4
orkit <ryan@dias.se>
2022-01-12 10:34:57 +0100

    Extended validity period for email partner invitations to 72 hours

--- resources/views/email/partner_invite.blade.php
+++ resources/views/email/partner_invite.blade.php
@@ -2,7 +2,7 @@ Dear Partner,
 
 This is to invite you to join {{config('app.name')}}
 
-Follow this link to register (This invite URL expires in 8 hours):
+Follow this link to register. Please note! This invite URL expires in 72 hours:
 
 {{$notification_url}}
 

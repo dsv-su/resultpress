18411290fa27d1aaca026a6ad0b0c501d375fa7f
Elias Chalhoub <el-ch@hotmail.com>
2022-12-21 10:06:05 +0100

    Refactor action links to work with the new features.

--- resources/views/project/action_links.blade.php
+++ resources/views/project/action_links.blade.php
@@ -1,17 +1,36 @@
-@can('project-'.$project->id.'-edit')
-    <a href="/project/{{$project->id}}/edit" data-toggle="tooltip" title="Edit this project" class="btn btn-outline-secondary btn-sm mb-1"><span
-                class="d-none d-sm-inline-block">Edit </span><i class="far fa-edit ml-sm-1"></i></a>
-    <a href="/project/{{$project->id}}/history" data-toggle="tooltip" title="Changes history" class="btn btn-outline-secondary btn-sm mb-1"><span
-                class="d-none d-sm-inline-block">History </span><i class="fas fa-history ml-sm-1"></i></a>
-@endcan
-@can('project-'.$project->id.'-update')
-    <a href="/project/{{$project->id}}/update" data-toggle="tooltip" title="Write an update" class="btn btn-outline-secondary btn-sm mb-1"><span
-                class="d-none d-sm-inline-block">Write an update </span><i class="fas fa-folder-plus ml-sm-1"></i></a>
-    <a href="/project/{{$project->id}}/updates" data-toggle="tooltip" title="Show all updates" class="btn btn-outline-secondary btn-sm mb-1"><span
-                class="d-none d-sm-inline-block">All updates </span><i class="far fa-list-alt ml-sm-1"></i></a>
-@endcan
-@can('project-'.$project->id.'-delete')
-    <a href="/project/{{$project->id}}/delete" data-toggle="tooltip" title="Delete this project" class="btn btn-outline-danger btn-sm mb-1"
-       onclick="return confirm('Are you sure you want to delete this item?');"><span class="d-none d-sm-inline-block">Delete </span><i
-                class="far fa-trash-alt ml-sm-1"></i></a>
+@php
+    $btnClass = 'btn btn-outline-secondary btn-sm mb-1';
+    $spanClass = 'd-none d-sm-inline-block';
+@endphp
+
+@if (Auth::user()->hasRole('Partner'))
+        @can('project-' . $project->id . '-edit')
+            <a href="/project/{{ $project->change_request->id ?? $project->id.'/edit' }}" title="Edit this project" class="{{ $btnClass }}"><span class="{{ $spanClass }}">{{ $project->object_type == 'project' ? $project->change_request ? 'Edit change request' : 'Add change request' : 'Edit' }}</span><i class="far fa-edit ml-sm-1"></i></a>
+
+            <a href="/project/{{ $project->id }}/history" title="Changes history" class="{{ $btnClass }}"><span class="{{ $spanClass }}">History </span><i class="fas fa-history ml-sm-1"></i></a>
+        @endcan
+
+@elseif (Auth::user()->hasRole(['Spider', 'Administrator']))
+    @can('project-' . $project->id . '-edit')
+        @if ($project->change_request && $project->change_request->id)
+            <a href="/project/{{ $project->change_request->id }}" title="Edit this project" class="{{ $btnClass }}"><span class="{{ $spanClass }}">Review change request</span><i class="far fa-edit ml-sm-1"></i></a>
+        @endif
+        <a href="/project/{{ $project->id }}/edit" title="Edit this project" class="{{ $btnClass }}"><span class="{{ $spanClass }}">Edit</span><i class="far fa-edit ml-sm-1"></i></a>
+
+        <a href="/project/{{ $project->id }}/history" title="Changes history" class="{{ $btnClass }}"><span class="{{ $spanClass }}">History </span><i class="fas fa-history ml-sm-1"></i></a>
+        
+        @if(in_array($project->object_type, ['project_add_request', 'project_change_request']))
+            <a href="/project/{{ $project->id }}/reject" title="Reject this project" class="btn btn-outline-danger btn-sm mb-1 ml-2 float-right" onclick="return confirm('Are you sure you want to reject this project?');"><span class="{{ $spanClass }}">Reject </span><i class="far fa-window-close ml-sm-1"></i></a>
+            <a href="/project/{{ $project->id }}/accept" title="Write an update" class="{{ $btnClass }} ml-4 float-right"><span class="{{ $spanClass }}">Accept</span><i class="fas fa-check-square ml-sm-1"></i></a>
+        @endif
+    @endcan
+
+@endif
+
+@if (auth()->user()->can('project-' . $project->id . '-update') && $project->object_type == 'project')
+    <a href="/project/{{ $project->id }}/update" title="Write an update" class="{{ $btnClass }}"><span class="{{ $spanClass }}">Write an update</span><i class="fas fa-folder-plus ml-sm-1"></i></a>
+    <a href="/project/{{ $project->id }}/updates" title="Show all updates" class="{{ $btnClass }}"><span class="{{ $spanClass }}">All updates </span><i class="far fa-list-alt ml-sm-1"></i></a>
+@endif
+@can('project-' . $project->id . '-delete')
+    <a href="/project/{{ $project->id }}/delete" title="Delete this project" class="btn btn-outline-danger btn-sm mb-1" onclick="return confirm('Are you sure you want to delete this item?');"><span class="{{ $spanClass }}">Delete </span><i class="far fa-trash-alt ml-sm-1"></i></a>
 @endcan

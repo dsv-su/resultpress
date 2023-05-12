be0610ca81d65dbe03498dcde02da8684c4abaea
Elio Ch <elio123@gmail.com>
2023-05-12 09:29:10 +0000

    Changed the logo and updated the nav menu changed summary textbox size.

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,17 +1,6 @@
 <div id="header">
-    <!--
-    <a id="header-su-responsive" href="http://www.su.se/english" title="Stockholm University home">
-        <img src="{{ asset('images/su_logo_responsive_en.png') }}" alt="Stockholm University"/>
+    <a id="header-dsv" href="{{ route('home') }}" title="Home" accesskey="1">
+        <img src="{{ asset('images/logo-2023.png') }}" alt="SPIDER">
     </a>
-    -->
-
-    <a id="header-dsv" href="#" title="Home" accesskey="1">
-        <img src="{{ asset('images/logo-2015-big-copy.png') }}" alt="SPIDER">
-    </a>
-<!--
-    <a id="header-su" href="http://www.su.se/english/" title="Stockholm University home">
-        <img src="{{ asset('images/su_logo_en.gif') }}" alt="Stockholm University">
-    </a>
-    -->
     <div class="clear"></div>
 </div>
--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -2,22 +2,23 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="{{ route('home') }}">ResultPress</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
+            <li class="nav-item">
+                @if (Auth::user()->setting == null)
+                    <a class="nav-link" href="{{ route('profile') }}">Dashbord<span class="badge badge-primary" style="z-index:15;position:relative; left: 0; top:-10px">1</span></a>
+                @else
+                    <a class="nav-link" href="{{ route('profile') }}">Dashbord</a>
+                @endif
+            </li>
             @can('project-create')
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('project_create') }}">Add</a>
                 </li>
             @endcan
-            <!--
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">MyProjects</a>
-            </li>
-            -->
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('search') }}">Projects</a>
             </li>
@@ -31,27 +32,15 @@
                 </li>
             @endif
             <li class="nav-item">
-                @if(Auth::user()->setting == null )
-                    <a class="nav-link" href="{{route('profile')}}">{{ Auth::user()->name ?? 'UserName' }}<span
-                                class="badge badge-primary"
-                                style="z-index:15;position:relative; left: 0; top:-10px">1</span></a>
-                @else
-                    <a class="nav-link" href="{{route('profile')}}">{{ Auth::user()->name ?? 'UserName' }}</a>
-                @endif
-            </li>
-            @can('partner')
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('partner-logout') }}"
-                       onclick="event.preventDefault();
+                <a class="nav-link" href="{{ route('partner-logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
+                    {{ __('Logout') }}
+                </a>
 
-                    <form id="logout-form" action="{{ route('partner-logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </li>
-            @endcan
+                <form id="logout-form" action="{{ route('partner-logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
             @can('project-create')
                 <li><a class="nav-link" href="{{ route('admin') }}">Admin</a></li>
             @endcan

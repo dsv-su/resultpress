1ad6bdcc4cfffbc1e785326c90721597f07ffb0b
Elias Chalhoub <el-ch@hotmail.com>
2023-05-16 10:21:56 +0200

    Allow administrators to view admin page based on role.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,7 @@ class AdminController extends Controller
 {
     public function index()
     {
-        if (!auth()->user()->hasPermissionTo('view-areas')) {
+        if (!auth()->user()->hasRole(['Administrator', 'Program administrator', 'Spider'])) {
             return redirect()->route('home')->withErrors(['You do not have permission to view this page.']);
         }
         return view('home.admin');

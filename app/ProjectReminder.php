0d563a8eccc7ae41386388a49db582d03d3cb8c0
Elias Chalhoub <el-ch@hotmail.com>
2023-05-23 15:59:56 +0200

    Add slugs for project reminder model.

--- app/ProjectReminder.php
+++ app/ProjectReminder.php
@@ -3,12 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class ProjectReminder extends Model
 {
+    use HasSlug;
+
     protected $fillable = ['project_id', 'name', 'reminder', 'reminder_due_days', 'set', 'type'];
     protected $cast = ['reminder_due_days' => 'integer'];
     protected $dates = ['set'];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(50)
+            ->doNotGenerateSlugsOnUpdate();
+    }
 
     /**
      * Get the project that owns the reminder.

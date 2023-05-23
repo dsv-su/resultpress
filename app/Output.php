53a9f71cf0bea18d9de6559f6071abbd6eded7d5
Elias Chalhoub <el-ch@hotmail.com>
2023-05-23 15:58:50 +0200

    Add slugs for output model.

--- app/Output.php
+++ app/Output.php
@@ -3,11 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
 
 class Output extends Model
 {
+    use HasSlug;
+
     protected $fillable = ['indicator', 'status', 'target', 'project_id'];
     protected $appends = ['valuesumnew', 'latest_update'];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('indicator')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(50)
+            ->doNotGenerateSlugsOnUpdate();
+    }
 
     /**
      * Get the project that owns the Output

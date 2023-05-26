0cc91555e9665cbbc7f297492f69683d2b64e9d2
Elias Chalhoub <el-ch@hotmail.com>
2023-05-26 15:55:19 +0200

    Test debug route, we need some info that we don't have access to.

--- app/User.php
+++ app/User.php
@@ -7,8 +7,9 @@ use Illuminate\Notifications\Notifiable;
 use Spatie\Activitylog\Traits\CausesActivity;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject as JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable, LogsActivity, CausesActivity, HasRoles;
 
@@ -43,6 +44,26 @@ class User extends Authenticatable
 
     protected static $logFillable = true;
     protected static $logOnlyDirty = true;
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /*public function project()
     {

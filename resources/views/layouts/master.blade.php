f3d36246f8cd5f81a5ac73bc482754c7cdb3fc92
Elias Chalhoub <el-ch@hotmail.com>
2023-05-31 12:11:00 +0200

    Add success message to master layout.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,6 +44,11 @@
                 </ul>
         </div>
     @endif
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
     <div id="contents">
         <a class="accessibility-link"></a>
         @yield('content')

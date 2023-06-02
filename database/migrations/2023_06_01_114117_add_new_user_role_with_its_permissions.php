f7d2961ae09f24a1f29297253aeefeb0750dbee4
Elias Chalhoub <el-ch@hotmail.com>
2023-06-02 13:13:50 +0200

    Add migration for regulators functionalities.

---
+++ database/migrations/2023_06_01_114117_add_new_user_role_with_its_permissions.php
@@ -0,0 +1,112 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\TaxonomyType;
+
+class AddNewUserRoleWithItsPermissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $permission = Permission::where('name', 'reviewer');
+        if (!$permission->count()) {
+            $permission = Permission::create([
+                'name' => 'reviewer',
+                'guard_name' => 'web'
+            ]);
+        }
+
+        // Add new user role with its permissions if not exists
+        $role = Role::where('name', 'Regulator Admin');
+        if (!$role->count()) {
+            $role = Role::create([
+                'name' => 'Regulator Admin',
+                'guard_name' => 'web'
+            ]);
+
+            $role->syncPermissions([
+                'project-list',
+                'project-create',
+                'project-edit',
+                'project-update',
+                'view-areas',
+                'reviewer',
+            ]);
+        }
+
+        $role = Role::where('name', 'Regulator User');
+        if (!$role->count()) {
+            $role = Role::create([
+                'name' => 'Regulator User',
+                'guard_name' => 'web'
+            ]);
+
+            $role->syncPermissions([
+                'project-list',
+                'project-create',
+                'project-edit',
+                'project-update',
+            ]);
+        }
+
+        // Create some basic taxonomy types
+        $types = ['Category', 'Region', 'Language', 'Regulators Area'];
+        foreach ($types as $type) {
+            $taxonomyType = TaxonomyType::where('name', $type);
+            if (!$taxonomyType->count()) {
+                $taxonomyType = TaxonomyType::create([
+                    'name' => $type,
+                    'description' => $type,
+                    'model' => $type == 'Regulators Area' ? 'Area' : null,
+                ]);
+
+                if ($type == 'Regulators Area' && $taxonomyType->taxonomies()->count() == 0) {
+                    $taxonomyType->taxonomies()->create([
+                        'title' => 'Regulator Area',
+                        'slug' => 'regulator-area',
+                        'parent_id' => null,
+                    ]);
+                    $taxonomyType->taxonomies()->create([
+                        'title' => 'Not Regulator Area',
+                        'slug' => 'not-regulator-area',
+                        'parent_id' => null,
+                    ]);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Delete all permissions
+        $permissions = Permission::whereIn('name', [
+            'reviewer',
+        ])->get();
+        foreach ($permissions as $permission) {
+            $permission->delete();
+        }
+
+        // Delete all roles
+        $roles = Role::whereIn('name', [
+            'Regulator Admin',
+            'Regulator User',
+        ])->get();
+        foreach ($roles as $role) {
+            $role->delete();
+        }
+    }
+}
